ec7778b9517360ceb7e7ae217db5db2ec5d6e192
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-23 16:43:13 +0900

    Update 다운로드.py

--- 다운로드.py
+++ 다운로드.py
@@ -3,3 +3,6 @@ import webbrowser
 webbrowser.open('https://www.sublimetext.com/')
 webbrowser.open('https://desktop.github.com/')
 webbrowser.open('https://github.com/naver/d2codingfont/releases/tag/VER1.3.2')
+
+import subprocess
+subprocess.run(['pip','install','pygame'], capture_output=True)
