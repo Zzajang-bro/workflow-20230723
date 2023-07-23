6bbd5077d753a281fd7b9494b4a7a5411e2b6ba7
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-23 16:42:21 +0900

    Update and rename 다운로드.md to 다운로드.py

---
+++ 다운로드.py
@@ -0,0 +1,5 @@
+# https://www.python.org/
+import webbrowser
+webbrowser.open('https://www.sublimetext.com/')
+webbrowser.open('https://desktop.github.com/')
+webbrowser.open('https://github.com/naver/d2codingfont/releases/tag/VER1.3.2')
