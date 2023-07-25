1213b909c54204e894f5eef0a9a2b37644df2300
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-25 19:38:40 +0900

    Update 다운로드.py

--- 다운로드.py
+++ 다운로드.py
@@ -9,7 +9,7 @@ webbrowser.open('https://www.pygame.org/docs/')
 webbrowser.open('https://www.msys2.org/')
 webbrowser.open('https://github.com/Zzajang-bro/msys-mingw64-init-20230725/blob/main/msys-mingw64-init-20230725.sh')
 webbrowser.open('https://gist.github.com/thales17/fb2e4cff60890a51d9dddd4c6e832ad2')
-
+# https://github.com/AutoDarkMode/Windows-Auto-Night-Mode/releases/tag/10.3.0.90
 
 import subprocess
 subprocess.run(['pip','install','pygame'], capture_output=True)
