1e366ab6701988c86d48bd2ed7dcb581123d03e7
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-25 19:20:16 +0900

    Update 다운로드.py

--- 다운로드.py
+++ 다운로드.py
@@ -8,6 +8,7 @@ webbrowser.open('https://github.com/naver/d2codingfont/releases/tag/VER1.3.2')
 webbrowser.open('https://www.pygame.org/docs/')
 webbrowser.open('https://www.msys2.org/')
 webbrowser.open('https://github.com/Zzajang-bro/msys-mingw64-init-20230725/blob/main/msys-mingw64-init-20230725.sh')
+webbrowser.open('https://gist.github.com/thales17/fb2e4cff60890a51d9dddd4c6e832ad2')
 
 
 import subprocess
